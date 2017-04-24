1eb754276dd5bc13bd67649f3b51568b3c0ae6ac
Marius <mariusvalentin.stan@gmail.com>
2017-04-24 10:14:07 +0300

    Updated the template data.

--- TemplateData/CanvasResize.js
+++ TemplateData/CanvasResize.js
@@ -1,11 +1,13 @@
 window.addEventListener("load", onLoaded);
 window.addEventListener("resize", resizeCanvas);
 
-var CANVAS_WIDTH_MIN = 1600;
-var CANVAS_WIDTH_MAX = 1920;
+var CANVAS_WIDTH_MIN = 1600.0;
+var CANVAS_WIDTH_MAX = 1920.0;
 
-var CANVAS_HEIGHT_MIN = 900;
-var CANVAS_HEIGHT_MAX = 1080;
+var CANVAS_HEIGHT_MIN = 900.0;
+var CANVAS_HEIGHT_MAX = 1080.0;
+
+const ASPECT_RATIO = CANVAS_WIDTH_MAX / CANVAS_HEIGHT_MAX;
 
 function onLoaded()
 {
@@ -34,8 +36,17 @@ function resizeCanvas()
 	
 	console.log("resizeCanvas() width: " + width + ", height: " + height);
 	
-	var widthClamped = ClampMinMax(width, CANVAS_WIDTH_MIN, CANVAS_WIDTH_MAX);
-	var heightClamped = ClampMinMax(height, CANVAS_HEIGHT_MIN, CANVAS_HEIGHT_MAX);
+	var widthClamped = ValueMin(width, CANVAS_WIDTH_MAX);
+	var heightClamped = ValueMin(height, CANVAS_HEIGHT_MAX);
+	if((widthClamped < CANVAS_WIDTH_MIN) || ((heightClamped < CANVAS_HEIGHT_MIN)))
+	{
+		widthClamped = heightClamped * ASPECT_RATIO;
+		if(widthClamped > width)
+		{
+			widthClamped = width;
+			heightClamped = widthClamped / ASPECT_RATIO;
+		}
+	}
 	
 	var canvasElement = document.getElementById("canvas");
 	if(canvasElement != null)
@@ -45,14 +56,20 @@ function resizeCanvas()
 			canvasElement.style.width = widthClamped + "px";
 			canvasElement.style.height = heightClamped + "px";
 			
+			var canvasFrame = document.getElementById("canvasFrame");
+			if(canvasFrame != null)
+			{
+				var scaleFactorX = widthClamped / CANVAS_WIDTH_MAX;
+				var scaleFactorY = heightClamped / CANVAS_HEIGHT_MAX;
+				canvasFrame.style.transform = "scale(" + scaleFactorX + ", " + scaleFactorY + ")";
+			}
+			
 			console.log("resizeCanvas() widthClamped: " + widthClamped + ", heightClamped: " + heightClamped);
 		}
 	}
 }
 
-function ClampMinMax(value, minValue, maxValue)
+function ValueMin(a, b)
 {
-	var clampedValue = (value < minValue) ? minValue: value;
-	clampedValue = (clampedValue > maxValue) ? maxValue: clampedValue;
-	return clampedValue;
+	return (a < b) ? a: b;
 }
